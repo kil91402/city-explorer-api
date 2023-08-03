baa79af02b9c3cba6f68e46ca8858f10745539ef
root <root@DESKTOP-RAKF29J>
2023-08-02 21:37:10 -0500

    changes added

--- weather.js
+++ weather.js
@@ -1,18 +1,51 @@
-// Weather.js
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Weather from "./weather";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./server";
 
-const Weather = ({ forecasts }) => {
+const App = () => {
+  const [city, setCity] = useState("");
+  const [forecasts, setForecasts] = useState([]);
+
+  const handleCitySearch = async () => {
+    try {
+      const response = await axios.get("/weather", {
+        params: {
+          searchQuery: city,
+        },
+      });
+
+      setForecasts(response.data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
   return (
-    <div>
-      <h3>Weather Forecast</h3>
-      <ul className="list-group">
-        {forecasts.map((forecast, index) => (
-          <li key={index} className="list-group-item">
-            <strong>Date:</strong> {forecast.date},{" "}
-            <strong>Description:</strong> {forecast.description}
-          </li>
-        ))}
-      </ul>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter city name"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={handleCitySearch}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {forecasts.length > 0 && <Weather forecasts={forecasts} />}
     </div>
   );
 };
