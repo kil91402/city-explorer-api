147f4cead82eb7947945670cbfce304353182f77
root <root@DESKTOP-RAKF29J>
2023-08-04 16:44:31 -0500

    changes added

--- server.js
+++ server.js
@@ -8,27 +8,14 @@ app.use(express.json());
 
 const weatherData = JSON.parse(fs.readFileSync("data/weather.json", "utf8"));
 
+// ... (existing code)
+
 class Forecast {
   constructor(date, description) {
     this.date = date;
     this.description = description;
   }
 }
-
-// app.get("/weather", (req, res) => {
-//   const { lat, lon, searchQuery } = req.query;
-
-//
-//   );
-
-//   if (!cityData) {
-//     return res.status(404).json({ error: "City not found" });
-//   }
-
-// const forecasts = cityData.data.map((data) => {
-//   const forecast = new Forecast(data.datetime, data.weather.description);
-//   return forecast;
-// });
 
 app.get("/", (request, response) => {
   response.send("Hello World");
@@ -38,7 +25,10 @@ app.get("/weather", async (req, res) => {
   const { locationLat, locationLon, searchQuery } = req.query;
   const cityData = weatherData.find(
     (city) =>
-      (locationLat && locationLon && city.lat === lat && city.lon === lon) ||
+      (locationLat &&
+        locationLon &&
+        city.lat === locationLat &&
+        city.lon === locationLon) ||
       (searchQuery &&
         city.city_name.toLowerCase() === searchQuery.toLowerCase())
   );
@@ -49,21 +39,11 @@ app.get("/weather", async (req, res) => {
 
   try {
     const weatherApiKey = process.env.WEATHER_API_KEY;
-    //const apiUrl = `https://api.weatherbit.io/v2.0/forecast/daily`;
     const apiResponse = await axios.get(
-      `https://api.weatherbit.io/v2.0/forecast/daily/${process.env.WEATHER_API_KEY}/${req.query.data.lat},${req.query.data.lon}`,
-      {
-        params: {
-          city_name: city_name,
-          locationLat: lat,
-          locationLon: lon,
-          key: process.env.WEATHER_API_KEY,
-          days: 3,
-        },
-      }
+      `https://api.weatherbit.io/v2.0/forecast/daily?lat=${locationLat}&lon=${locationLon}&key=${weatherApiKey}&days=3`
     );
 
-    const forecast = apiResponse.data.forecast.map((item) => {
+    const forecast = apiResponse.data.data.map((item) => {
       return new Forecast(item.datetime, item.weather.description);
     });
 
@@ -73,7 +53,6 @@ app.get("/weather", async (req, res) => {
     return res.status(500).json({ error: "Failed to fetch weather data." });
   }
 });
-//});
 
 app.listen(3000, () => {
   console.log("Listen on the port 3000...");
